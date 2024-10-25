962b4ecacd6cd53b78dea02b8d908516933f97e5
Bùi Khánh Duy <105474344+kduy138@users.noreply.github.com>
2024-10-25 12:41:11 +0700

    them

--- DoAnChuyenNganh_HeThongTrungTamTinHoc/Javascript/GiangVien.js
+++ DoAnChuyenNganh_HeThongTrungTamTinHoc/Javascript/GiangVien.js
@@ -1,4 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Dữ liệu học viên
+    const hocVienData = [
+        { anh: "User.jpg", maHocVIen: "2001215669", tenHocVien: "Bùi Khánh Duy", email: "buikhanhduy13082003@gmail.com"},
+    ]
+
+    const thongTinHocVien = document.getElementById("thongTinHocVien");
+
+    hocVienData.forEach(hocvien => {
+        const row = document.createElement("tr");
+
+        const subjectCell = document.createElement("td");
+        subjectCell.textContent = hocvien.anh;
+        row.appendChild(subjectCell);
+
+        const timeCell = document.createElement("td");
+        timeCell.textContent = hocvien.maHocVIen;
+        row.appendChild(timeCell);
+
+        const roomCell = document.createElement("td");
+        roomCell.textContent = hocvien.tenHocVien;
+        row.appendChild(roomCell);
+
+        const emailCell = document.createElement("td");
+        emailCell.textContent = hocvien.email;
+        row.appendChild(emailCell);
+
+        thongTinHocVien.appendChild(row);
+    });
+
     // Dữ liệu giả lập cho lịch dạy
     const scheduleData = [
         { subject: "Lập trình Java", time: "Thứ 2 - 08:00 - 10:00", room: "Phòng A101" },
