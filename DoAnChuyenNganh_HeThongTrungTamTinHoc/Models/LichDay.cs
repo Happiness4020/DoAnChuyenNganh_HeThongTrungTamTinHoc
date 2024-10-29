c2ef82424db4de3668522622c5684e5d8ec103d9
Van <tran18218@gmail.com>
2024-10-29 22:48:14 +0700

    giao diện giáo viên 29/10

---
+++ DoAnChuyenNganh_HeThongTrungTamTinHoc/Models/LichDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnChuyenNganh_HeThongTrungTamTinHoc.Models
+{
+    public class LichDay
+    {
+        public DateTime Ngay { get; set; }
+        public string Gio { get; set; }
+        public string MonHoc { get; set; }
+        public string PhongHoc { get; set; }
+    }
+}
