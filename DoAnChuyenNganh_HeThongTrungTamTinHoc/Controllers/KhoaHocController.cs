caa430f76a0c6d8d014ddc94efc9f3ceeafd4d16
Bùi Khánh Duy <105474344+kduy138@users.noreply.github.com>
2024-11-06 16:54:23 +0700

    Thêm chức năng và chỉnh sửa giao diện
    Thêm chức năng phân trang và sắp xếp cho trang Khóa học. Thêm chức năng phân trang và sắp xếp cho trang Khóa học theo Chương trình học. Chỉnh sửa giao diện của 2 trang trên.

--- DoAnChuyenNganh_HeThongTrungTamTinHoc/Controllers/KhoaHocController.cs
+++ DoAnChuyenNganh_HeThongTrungTamTinHoc/Controllers/KhoaHocController.cs
@@ -11,13 +11,33 @@ namespace DoAnChuyenNganh_HeThongTrungTamTinHoc.Controllers
     {
         TrungTamTinHocEntities db = new TrungTamTinHocEntities();
         // GET: KhoaHoc
-        public ActionResult Index(string search = "")
+        public ActionResult Index(string search = "", int page = 1, string sort_by = "price_asc")
         {
             var khs = db.KhoaHoc;
             List<KhoaHoc> khoahocs = db.KhoaHoc.Where(e => e.TenKH.Contains(search)).ToList();
             ViewBag.Search = search;
             List<ChuongTrinhHoc> cths = db.ChuongTrinhHoc.ToList();
             ViewBag.ChuongTrinhHocs = cths;
+
+            // Sắp xếp
+            if (sort_by == "price_asc")
+            {
+                khoahocs = khoahocs.OrderBy(c => c.HocPhi).ToList();
+            }
+            else if (sort_by == "price_desc")
+            {
+                khoahocs = khoahocs.OrderByDescending(c => c.HocPhi).ToList();
+            }
+            ViewBag.SortBy = sort_by;
+
+            // Phân trang
+            int NumberOfRecordsPerPage = 8;
+            int NumberOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(khoahocs.Count) / Convert.ToDouble(NumberOfRecordsPerPage)));
+            int NumberOfRecordsToSkip = (page - 1) * NumberOfRecordsPerPage;
+            ViewBag.Page = page;
+            ViewBag.NumberOfPages = NumberOfPages;
+            khoahocs = khoahocs.Skip(NumberOfRecordsToSkip).Take(NumberOfRecordsPerPage).ToList();
+
             return View(khoahocs);
         }
 
